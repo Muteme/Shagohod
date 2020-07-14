1109caf7867a8a14d91064956216ede28010fb9e
Max <chaidclark@gmail.com>
2020-07-14 01:29:58 -0700

    First commit

--- Shagohod.py
+++ Shagohod.py
@@ -6,14 +6,19 @@ from fake_useragent import UserAgent
 from pprint import pprint
 from bs4 import BeautifulSoup
 from pyfiglet import Figlet
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 #Fake User Agent
 ua  = UserAgent()
-print(ua.random)
+#print(ua.random)
 
 #Headers
 headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,app$",
     "Accept-Encoding": "gzip, deflate", 
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8", 
     "Dnt": "1", 
@@ -23,14 +28,23 @@ headers = {
     "User-Agent": str(ua.random), 
   }
 
+#Fake-Browser
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.binary_location = '/usr/bin/google-chrome-stable'
+
+
 #Banner
 custom_fig = Figlet(font='cybermedium')
 print(custom_fig.renderText('Shagohod'))
+message = 'Your random header'
+#print(message + ua.random)
 
 #Site Input
-URL = input("What site would you like to scrape?[http://example.com]")
+URL = input("What site would you like to scrape?[http://www.example.com]")
 html_text = requests.get(URL, headers=headers)
 soup = BeautifulSoup(html_text.content, 'html.parser')
 
 #Print
 print (vars(soup))
+
